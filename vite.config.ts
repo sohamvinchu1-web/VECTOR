3ae7309e0a2aad4e48d7be46a8fa3083403f10de
sohamvinchu1-web <sohamvinchu1@gmail.com>
2026-05-29 23:35:40 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -8,9 +8,11 @@ import { viteSingleFile } from "vite-plugin-singlefile";
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-// https://vite.dev/config/
 export default defineConfig({
+  base: "/VECTOR/",
+
   plugins: [react(), tailwindcss(), viteSingleFile()],
+
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "src"),
